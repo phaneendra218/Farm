044e82b35fe95c9c56193ad5f895afaa0544b377
Phaneendra Raju <phaneendra@venusgeo.com>
2024-12-27 17:37:05 +0530

    address profile11

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, url_for, flash
+from flask import Flask, render_template, request, redirect, session, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 from werkzeug.utils import secure_filename
